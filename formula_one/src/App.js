375e6ba67d62c656884899ebb71dc3eb5dacb947
T2sha <21100474@virtualwindow.co.za>
2022-03-19 14:09:26 +0200

    Hide and show components

--- formula_one/src/App.js
+++ formula_one/src/App.js
@@ -2,13 +2,18 @@ import Header from "./components/Header"
 import Dashboard from "./components/Dashboard"
 import Compare from "./components/Compare"
 import Timeline from "./components/Timeline"
+import {Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <Routes>
       < Header />
+      <Route path="/" element = {<Dashboard />}/>
+      <Route path="/Compare" element = {<Compare />}/>
+      <Route path="/Timeline" element = {<Timeline />}/>
 
-     
+      </Routes>
     </div>
   );
 }
